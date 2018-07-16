e2540e3d51d8bf5c74c9584b29dd1b255666822b
SKRIABINE Sophie <sophie.skriabine@icm-renie-lp01.icm-institute.org>
2018-07-16 14:47:31 +0200

    basic autoencoder

---
+++ autoencoder.py
@@ -0,0 +1,68 @@
+import os
+
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+
+if not os.path.exists('./mlp_img'):
+    os.mkdir('./mlp_img')
+
+
+
+num_epochs = 100
+batch_size = 128
+learning_rate = 1e-3
+
+
+
+#autoencoder test
+class autoencoder(nn.Module):
+    def __init__(self):
+        super(autoencoder, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(True),
+            nn.Linear(128, 64),
+            nn.ReLU(True), nn.Linear(64, 12), nn.ReLU(True), nn.Linear(12, 3))
+        self.decoder = nn.Sequential(
+            nn.Linear(3, 12),
+            nn.ReLU(True),
+            nn.Linear(12, 64),
+            nn.ReLU(True),
+            nn.Linear(64, 128),
+            nn.ReLU(True), nn.Linear(128, 28 * 28), nn.Tanh())
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+
+
+
+def main(dataloader):
+    model = autoencoder().cuda()
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(
+        model.parameters(), lr=learning_rate, weight_decay=1e-5)
+
+    for epoch in range(num_epochs):
+        for data in dataloader:
+            img, _ = data
+            img = img.view(img.size(0), -1)
+            img = Variable(img).cuda()
+            # ===================forward=====================
+            output = model(img)
+            loss = criterion(output, img)
+            # ===================backward====================
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        # ===================log========================
+        print('epoch [{}/{}], loss:{:.4f}'
+              .format(epoch + 1, num_epochs, loss.data[0]))
+        if epoch % 10 == 0:
+            # check the output
+            print 'save picture'
+        torch.save(model.state_dict(), './sim_autoencoder.pth')
