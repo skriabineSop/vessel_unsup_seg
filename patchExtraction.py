6fa2c8bccf9a0f009254099b72a4376b876a9bde
paul.bertin <paul.f.bertin@gmail.com>
2018-07-16 17:56:37 +0200

    Optimized patch extraction

--- patchExtraction.py
+++ patchExtraction.py
@@ -19,7 +19,7 @@ def from_tif_to_patch(file):
         for j in range(0, image.shape[1] - patchShape[1], patchShape[1]):
             for k in range(0, image.shape[2] - patchShape[2], patchShape[2]):
                 cpt += 1
-                np.save(os.path.join(savedir, file[:-8] + '_patch_' + str(i) + '_' + str(j) + '_' + str(k)),
+                np.save(os.path.join(savedir, file[:-8], file[:-8] + '_patch_' + str(i) + '_' + str(j) + '_' + str(k)),
                         image[i:i + patchShape[0],
                         j:j + patchShape[1],
                         k:k + patchShape[2]])
@@ -28,11 +28,11 @@ def from_tif_to_patch(file):
                 #       (image.shape[2] // patchShape[2]))
 
 
-for files in os.listdir(workdir):
-    if '.tif' in files:
-        print(files)
-        from_tif_to_patch(files)
+for file in os.listdir(workdir):
+    if '.tif' in file:
+        print(file[:-8])
 
+        if not os.path.exists(os.path.join(savedir, file[:-8])):
+            os.makedirs(os.path.join(savedir, file[:-8]))
 
-
-
+        from_tif_to_patch(file)
