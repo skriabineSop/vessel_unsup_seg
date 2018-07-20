fixed minor bugs in the kernel generation + in soft cut loss replace the sum of the weights by a convolution on ones matrices in the computation of the soft cut loss

autoencoder.py
@@ -8,30 +8,22 @@ from visualize import plot3d, show
 import numpy as np
 from tensorboardX import SummaryWriter
 import torch.nn.functional as F
-from gaussianKernel import GaussianKernel
+from gaussianKernel import GaussianKernel, get_gaussian_filter
 from soft_cut_loss import Soft_cut_loss
 
 num_epochs = 100
 batch_size = 128
 learning_rate = 1e-3
 
-# datadir = '/mnt/raid/UnsupSegment/patches/10-43-24_IgG_UltraII[02 x 05]_C00' # ordi fixe
-datadir = '/home/paul.bertin/PycharmProjects/vessel_unsup_seg/data/toyDataset' # ordi perso
-logdir = 'logs/training190718_1'
+datadir = '/mnt/raid/UnsupSegment/patches/10-43-24_IgG_UltraII[02 x 05]_C00' # ordi fixe
+# datadir = '/home/paul.bertin/PycharmProjects/vessel_unsup_seg/data/toyDataset' # ordi perso
+logdir = 'logs/training200718_1'
 savedmodeldir = 'savedModels'
 sigma = 4
 kernel_size = 11
 Lambda = 1  # Used to weight relative importance of reconstruction loss and min cut loss
 
 writer = SummaryWriter(logdir)
-
-
-def get_gaussian_filter(sigma, kernel_size):
-    if torch.cuda.is_available():
-        gauss_filter = GaussianKernel(sigma, kernel_size).kernel.float().cuda()
-    else:
-        gauss_filter = GaussianKernel(sigma, kernel_size).kernel.float()
-    return gauss_filter
 
 
 def soft_cut_loss(x, kernel):
@@ -70,7 +62,7 @@ class autoencoder(nn.Module):
             nn.Conv3d(32, 16, 3, stride=1, padding=(1, 1, 1)),
             nn.ReLU(True),
             nn.Conv3d(16, 2, 3, stride=1, padding=(1, 1, 1)),
-            # nn.Softmax(dim=1)
+            nn.Softmax(dim=1)
             )
 
         self.decoder = nn.Sequential(
@@ -115,12 +107,17 @@ def main():
     # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
     print('parameters', model.parameters())
     # optimizer_encoder = torch.optim.SGD(model.encoder.parameters(), lr=1e-6)
-    optimizer_model = torch.optim.SGD(model.parameters(), lr=1e+4)
+    optimizer_model = torch.optim.SGD(model.parameters(), lr=1e-1)
     print("begin training")
     num_iteration = 0
 
+    cpt = 0
+
     for epoch in range(num_epochs):
         for data in dataloader:
+
+            cpt += 1
+
             if torch.cuda.is_available():
                 img = data.float().cuda()
             else:
@@ -134,7 +131,10 @@ def main():
             soft_loss = criterion2(model.intermediate, model.soft_cut_kernel)
             # loss = reconstruction_loss + Lambda * model.soft_cut_loss
             # loss = model.soft_cut_loss
-            loss = soft_loss
+            if cpt < 1200:
+                loss = reconstruction_loss
+            else:
+                loss = soft_loss
 
             # ===================get infos=====================
             writer.add_scalar('Train/Loss', loss, num_iteration)
@@ -155,13 +155,13 @@ def main():
             # ===================backward====================
             optimizer_model.zero_grad()
             loss.backward()
-
+            #
             print('loss', loss)
-            for name, param in model.named_parameters():
-                if param.requires_grad:
-                    print(name, param.data[0])
-                    print("grad", param.grad[0])
-                    break
+            # for name, param in model.named_parameters():
+            #     if param.requires_grad:
+            #         print(name, param.data[0])
+            #         print("grad", param.grad[0])
+            #         break
             optimizer_model.step()
 
             # loss = reconstruction_loss

soft_cut_loss.py
@@ -8,22 +8,52 @@ from visualize import plot3d, show
 import numpy as np
 from tensorboardX import SummaryWriter
 import torch.nn.functional as F
-
+from gaussianKernel import get_gaussian_filter
+logdir = 'logs'
 
 def normalizedSoftCutLoss(input, kernel):
+    ones = torch.from_numpy(np.ones((1, 2, 40, 40, 40))).cuda().float()
+    res = torch.from_numpy(np.zeros(1)).cuda().float()
+    print("res", res)
     for i in range(input.shape[1]):
         gauss = F.conv3d(input[:, i:i + 1, :, :, :],
                                   kernel[np.newaxis, :], bias=None, stride=1, padding=(5, 5, 5))
-        gauss =gauss
+        print("gauss", gauss.shape, gauss)
+        #np.save(os.path.join(logdir, 'gauss' + 'test', gauss.data[0, 0]))
+        # plot3d(np.array(input.data[0,0]).reshape((40, 40, 40)))
+        # show()
+        # plot3d(np.array(gauss.data).reshape((40, 40, 40)))
+        # show()
+        print("input", input.shape)
         mul = torch.mul(input[:, i:i + 1, :, :, :], gauss)
+        # plot3d(np.array(gauss.data).reshape((40, 40, 40)))
+        # show()
+        print("mul", mul.shape)
+        print("mul", mul)
+
         numerator = torch.sum(mul)
-        sum_gauss = torch.sum(gauss)
+
+        print("numerator", numerator)
+
+        # sum_gauss = torch.sum(kernel)
+        sum_gauss = F.conv3d(ones[:, i:i + 1, :, :, :],
+                         kernel[np.newaxis, :], bias=None, stride=1, padding=(5, 5, 5))
+
+        print("sum gauss", sum_gauss)
+
         sub_denom = torch.mul(input[:, i:i + 1, :, :, :], sum_gauss)
+
+        print("sub denom", sub_denom)
+
         denom = torch.sum(sub_denom)
-        res = numerator / denom
+
+        print("denom", denom)
+
+        res += numerator / denom
+
+        print("res", res)
 
     result =torch.add(torch.mul(res, -1), input.shape[1])
-    print('result :', res, result.type())
     return result
 
 
@@ -33,8 +63,24 @@ class Soft_cut_loss(nn.Module):
         super(Soft_cut_loss, self).__init__()
 
     def forward(self, x, kernel):
-        print('compute soft loss')
         return normalizedSoftCutLoss(x, kernel)
 
 
+if __name__ == "__main__":
 
+    ones = np.ones((1, 2, 20, 40, 40))
+    zeros = np.zeros((1, 2, 20, 40, 40))
+    latent = np.concatenate((ones, ones), axis=2)
+    latent = torch.from_numpy(latent).cuda().float()
+
+    criterion2 = Soft_cut_loss()
+
+    # print(np.unique(latent[0, 1, :, :, 0]))
+
+    sigma = 4
+    kernel_size = 11
+    kernel = get_gaussian_filter(sigma, kernel_size)
+
+    soft_loss = criterion2(latent, kernel)
+
+    print(soft_loss)

visualize.py
@@ -86,22 +86,20 @@ def reconstructionView(readdir, N):
 
 if __name__ == '__main__':
 
-    readdir = 'logs/training190718_1'
+    readdir = 'logs/training200718_1'
     N = 1000
 
     reconstructionView(readdir, N)
 
     input = np.load(os.path.join(readdir, 'input_' + str(N) + '.npy'))
     latent1 = np.load(os.path.join(readdir, 'latent1_' + str(N) + '.npy'))
-    #latent2 = np.load(os.path.join(readdir, 'latent2_' + str(N) + '.npy'))
+    latent2 = np.load(os.path.join(readdir, 'latent2_' + str(N) + '.npy'))
 
-    plot3d(np.load(os.path.join(readdir, 'kernel_' + str(N) + '.npy')))
-    show()
 
     print('latent1 min max', np.min(latent1), np.max(latent1))
-    #print('latent2 min max', np.min(latent2), np.max(latent2))
+    print('latent2 min max', np.min(latent2), np.max(latent2))
 
     vb1, vb2 = get_two_views()
     plot3d(latent1, view=vb1)
-    plot3d(input, view=vb2)
+    plot3d(latent2, view=vb2)
     show()